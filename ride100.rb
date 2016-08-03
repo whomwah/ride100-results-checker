2ddb00b995cb59e7fd3c1b89defc6f02e9d67c2a
Duncan Robertson <duncan@whomwah.com>
2016-08-03 09:32:14 +0100

    Create ride100.rb

---
+++ ride100.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require 'open-uri'
+require 'nokogiri'
+
+class RideLondon
+
+  def initialize
+    @current_page = 1
+    @url = 'http://results.prudentialridelondon.co.uk'
+    @year = '2016'
+  end
+
+  def process
+    CSV.open("ridelondon100.csv", "wb") do |csv|
+      csv << headers
+
+      while @current_page
+        scrape!
+
+        riders.each do |rider|
+          details = rider_details(rider)
+          if details[4] == '100 Miles'
+            csv << details[0...headers.size]
+          end
+        end
+
+        @current_page += 1
+        sleep 1
+
+        if no_more_results?
+          @current_page = nil
+        end
+      end
+    end
+  end
+
+  def scrape!
+    puts "Checking: Page#{@current_page}"
+    html = open(url)
+
+    if html
+      @doc = Nokogiri::HTML(html)
+    end
+  end
+
+  private
+
+  def headers
+    ["Number", "Name", "AG", "Club", "Distance", "EST MILE 17", "EST MILE 26", "EST MILE 47", "EST MILE 55", "EST MILE 75", "EST MILE 85", "FINISH"]
+  end
+
+  def url
+    File.join(@url, @year) + "?page=#{@current_page}&event=I&event_main_group=A&num_results=100&pid=list&search"
+  end
+
+  def no_more_results?
+    message = @doc.css('.results-message').first
+    !!(message && message.content == 'There are currently no results available.')
+  end
+
+  def riders
+    @doc.css('table.list-table tbody tr')
+  end
+
+  def rider_details(rider)
+    rider.css('td').map { |data| data.content }
+  end
+end
+
+RideLondon.new.process
